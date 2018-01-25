589c836bb9ccf9cf4fe905e7a7e2d197a096f236
郭鹏飞 <798771558@qq.com>
2018-01-25 13:44:28 +0800

    添加全局异常处理（未实现）

--- src/main/java/com/shuttle/bus/gpf/core/mantain/controller/UserController.java
+++ src/main/java/com/shuttle/bus/gpf/core/mantain/controller/UserController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shuttle.bus.gpf.core.api.entity.User;
+import com.shuttle.bus.gpf.core.api.entity.dto.ResultDto;
 import com.shuttle.bus.gpf.core.api.service.UserService;
 
 /**
  * 用户操作控制器
+ * <blockquote><pre>
+ * 	此类所有端口方法返回结果方式均为json方式
+ * </pre></blockquote>
  * @author 郭鹏飞
  * @since 2018-01-20
  * @version 1.0
@@ -24,9 +28,17 @@ public class UserController {
 	private UserService userService;
 
 	@RequestMapping(value = "userTest")
-	public List<User> userTest(){
-		List<User> list = userService.getAllUser();
-		return list;
+	public ResultDto userTest(){
+		List<User> list;
+		try {
+			list = userService.getAllUser();
+			if(list == null || list.size() <= 0 )
+				return new ResultDto(2002,"查询到数据为空");
+			return new ResultDto(200,"成功",list);
+		} catch (Exception e) {
+			return new ResultDto(4003,"服务器发生异常哦");
+		}
+		
 	}
 	
 }
---
+++ src/main/java/com/shuttle/bus/gpf/core/mantain/global/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.shuttle.bus.gpf.core.mantain.global;
+
+/**
+ * 全局异常处理类
+ * 
+ * @author 郭鹏飞
+ *
+ */
+public class GlobalExceptionHandler {
+
+}
